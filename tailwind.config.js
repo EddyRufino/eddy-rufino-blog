43ce17a29fe022b49df22cfba1f7f6aaca61d692
Eddy Rufino <eddyjaair@gmail.com>
2020-06-08 19:39:34 -0500

    Haciendo el nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     './resources/css/**/*.css',
   ],
   theme: {
-    extend: {}
+    extend: {
+      display: ['responsive', 'hover', 'focus'],
+    },
+    screens: {
+      'xm': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    }
   },
   variants: {},
   plugins: [
